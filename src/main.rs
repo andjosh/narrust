50b15d8b3f538206c0122c7d08d78fe99692da0f
andjosh <email@andjosh.com>
2015-08-23 15:49:54 -0500

    Trying out rss request and parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate rss;
+extern crate request;
+
+use rss::Rss;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+
+    let url = "http://www.andjosh.com/rss.xml";
+    let mut headers: HashMap<String, String> = HashMap::new();
+    headers.insert(
+        "Connection".to_string(),
+        "close".to_string()
+    );
+
+    let resp = match request::get(&url, &mut headers) {
+        Ok(resp) => resp,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+
+    let rss_str = resp.body;
+    let Rss(channel) = rss_str.parse::<Rss>().unwrap();
+
+    println!("{}", res.status_code);
+    println!("{}", channel.title);
 }
